722a28f1ad5f43bbc6d92b8c76880608eb33ff5a
Chris Lin <chrislin@utexas.edu>
2018-03-07 23:40:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,29 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+var index = 0;
+
+
+document.body.onkeydown = function(event){
+  // console.log('hello');
+  // console.log(this);
+  console.log(event.which);
+  keyPressed = parseInt(event.which);
+
+  if (keyPressed === code[index]) {
+    index++;
+    console.log(index);
+
+    if (index === code.length) {
+      alert('correct');
+      index = 0;
+    } 
+
+  } else {
+    index = 0;
+  }
+
+};
+
 
 }
